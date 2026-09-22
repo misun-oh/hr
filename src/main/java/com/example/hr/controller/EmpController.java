52d44d15125d3cab3346b1fd3a026a02413bfc29
misun <momo.class55@gmail.com>
2026-09-22 13:15:03 +0900

    pageDto 출력

--- src/main/java/com/example/hr/controller/EmpController.java
+++ src/main/java/com/example/hr/controller/EmpController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.hr.dto.EmpDto;
+import com.example.hr.dto.PageDto;
 import com.example.hr.service.EmpService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,9 @@ public class EmpController {
 		
 		//model.addAttribute("totalCnt", service.totalCnt());
 		service.selectByCond(model);
+		
+		model.addAttribute("pageDto", new PageDto(130));	
+		
 		return "/index";
 	}
 	
